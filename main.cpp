bb49a6cdc0d98840bdf44a94785884a0550d35cc
Ziyad <163886679+ZiyadAbdelkarim@users.noreply.github.com>
2025-09-21 20:20:34 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ inertial gyro(PORT7);
 
 
 void pre_auton(void) {
-  gyro.calibrate()
+  gyro.calibrate();
 }
 // Lateral movment
 double kp = 0.0;
@@ -40,7 +40,7 @@ int derivitive;
 int intergral=0;
 // Genreral info
 float wheel_diameter = 3.75;
-float pi = 3.14:
+float pi = 3.14;
 float wheel_circumfrence = wheel_diameter*pi;
 // Angular Movment
 double turn_kp = 0.0;
@@ -53,36 +53,40 @@ int turn_derivitive;
 int turn_intergral=0;
 
 bool enable_pid = true; 
-int PID_Drive(int desired_value, int desired_turn){
+void PID_Drive(int desired_value){
 
-  while(enabled_pid && error <=.05){
+  while(enable_pid){
 // Lateral displacement 
-  int turns=0;
+
 
   // Grabs the pos from left motors
   int LMB_pos = LMB.position(degrees);
   int LMM_pos = LMM.position(degrees);
-  int LMF_pos = LMF_pos.position(degrees);
+  int LMF_pos = LMF.position(degrees);
 
   // Grabs the pos from right motors
   int RMB_pos = RMB.position(degrees);
   int RMM_pos = RMM.position(degrees);
-  int RMF_pos = RMF_pos.position(degrees);
+  int RMF_pos = RMF.position(degrees);
 
   // Gets avg pos to see the distance traveled
-  int avg_pos_deg = (LMB_pos+LMM_pos+LMR_pos+RMB_pos+RMM_pos+RMR_pos)/2;
+  int avg_pos_deg = (LMB_pos+LMM_pos+LMF_pos+RMB_pos+RMM_pos+RMF_pos)/6;
 
-  int turns = avg_pos_deg/360
-  int avg_pos = turns*wheel_circumfrence;
+  float turns = avg_pos_deg/360.0;
+  float avg_pos = turns*wheel_circumfrence;
 
   // Checks error for porp
-  int error = avg_pos- desired_value;
+  error = desired_value-avg_pos;
 
   // Checks error for Intergral
-  intergral+= error;
+  if (fabs(error) < 50) {
+    intergral += error;
+} else {
+    intergral = 0;
+}
 
   // Checks error for derivitive
-  int derivitive = erro-prev_error
+  derivitive = error-prev_error;
   double motor_power = kp*error+kd*derivitive+ki*intergral;
   // Motors go forward at desired speed to reach their target dist
   LMB.spin(forward, motor_power, voltageUnits::volt);
@@ -92,17 +96,26 @@ int PID_Drive(int desired_value, int desired_turn){
   RMB.spin(forward, motor_power, voltageUnits::volt);
   RMM.spin(forward, motor_power, voltageUnits::volt);
   RMF.spin(forward, motor_power, voltageUnits::volt);
-    task::sleep(20);
+  task::sleep(20);
+  prev_error=error;
+    if (fabs(error) <= 5){
+      break;
+    }
   }
 
-  avg_pos=0;
+  
 }
-int PID_turn(int desired_turn){
-  while(enabled_pid && turn_error <=.05){
-    int avg_turn = gyro.rotation();
-    int turn_error = avg_turn-desired_turn;
-    turn_intergral+=turn_error;
-    int derivitive = turn_error-turn_prev_error;
+void PID_turn(int desired_turn){
+  while(enable_pid){
+
+    float avg_turn = gyro.rotation();
+    turn_error = desired_turn-avg_turn;
+      if (fabs(turn_error) < 50) {
+    turn_intergral += turn_error;
+    } else {
+    turn_intergral = 0;
+    }
+    turn_derivitive = turn_error-turn_prev_error;
     double turn_motor_power = turn_kp*turn_error+turn_kd*turn_derivitive+turn_ki*turn_intergral;
     LMB.spin(forward, turn_motor_power, voltageUnits::volt);
     LMM.spin(forward, turn_motor_power, voltageUnits::volt);
@@ -114,12 +127,16 @@ int PID_turn(int desired_turn){
 
     turn_prev_error=turn_error;
     task::sleep(20);
+     if (fabs(turn_error) <= 5){
+      break;
+    }
 }
   gyro.reset();
 }
 void autonomous(void) {
-  vex::task PID_IS_GOATED(PID_Drive)
-  vex::task PID_IS_GOATED(PID_turn)
+  PID_Drive(10);
+  PID_turn(90);
+
   
 
 
@@ -149,4 +166,4 @@ int main() {
     wait(100, msec);
   }
 }
-
+ 
